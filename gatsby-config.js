b1132b1e4ce18194ea037d88f66aba715811afb5
Roman Sorin <988244@students.mentorschools.org>
2019-09-23 09:21:29 -0400

    aa

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`
+    title: `rs Research`,
+    description: `rs Research`,
+    author: `@romansorin`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
