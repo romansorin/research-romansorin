428463fe3c675e71f243f68dd198b76a4dcdfb73
Roman Sorin <988244@students.mentorschools.org>
2019-10-21 09:55:26 -0400

    Removed firebase files, going to redo integration

--- firebase-admin-sdk.js
+++
@@ -1,8 +0,0 @@
-import * as admin from 'firebase-admin'
-
-const serviceAccount = require('./firebase-key.json')
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://research-34a17.firebaseio.com'
-})
