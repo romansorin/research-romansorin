ea93b98255f777abc6b37d2ca5ae8e920d291827
Roman Sorin <roman@romansorin.com>
2019-10-20 23:36:17 -0400

    Standard formatting

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,36 +2,36 @@ const config = require('gatsby-plugin-config').default
 
 module.exports = {
   siteMetadata: {
-    title: `rs Research`,
-    description: `rs Research`,
-    author: `@romansorin`
+    title: 'rs Research',
+    description: 'rs Research',
+    author: '@romansorin'
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`
       }
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
       }
     },
-    `gatsby-plugin-postcss`,
+    'gatsby-plugin-postcss',
     {
-      resolve: `gatsby-source-firebase`,
+      resolve: 'gatsby-source-firebase',
       options: {
         // point to the firebase private key downloaded
         credential: require('./firebase-key.json'),
