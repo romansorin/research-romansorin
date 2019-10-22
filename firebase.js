aa2e089da5711a70ed09dd3ff58e19fc89b39d7a
Roman Sorin <roman@romansorin.com>
2019-10-21 21:19:38 -0400

    Firebase config (props to yawnch)

--- firebase.js
+++ firebase.js
@@ -5,36 +5,22 @@ import 'firebase/analytics'
 import 'firebase/firestore'
 import 'firebase/storage'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCO605Sg-zHp3034Pwg7uBwcbT_VyVVX8E',
-  authDomain: 'research-romansorin.firebaseapp.com',
-  databaseURL: 'https://research-romansorin.firebaseio.com',
-  projectId: 'research-romansorin',
-  storageBucket: 'research-romansorin.appspot.com',
-  messagingSenderId: '520588087445',
-  appId: '1:520588087445:web:5c823d0bd1928d68e38b9c',
-  measurementId: 'G-BMXGTF3X91'
-}
+export const initializeFirebase = () => {
+  const config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    appId: process.env.APP_ID,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    measurementId: process.env.MEASUREMENT_ID,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
+  }
 
-firebase.initializeApp(firebaseConfig)
+  if (!firebase.apps.length) {
+    firebase.initializeApp(config)
+  }
+}
 
 export const database = firebase.firestore()
 export const storage = firebase.storage()
-
-// Config from yawnch RFSSR
-// import firebase from 'firebase/app'
-// import 'firebase/auth'
-
-// export const initializeFirebase = () => {
-//   const config = {
-//     apiKey: process.env.FIREBASE_API_KEY,
-//     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//     databaseURL: process.env.FIREBASE_DATABASE_URL,
-//     // storageBucket: process.env.STORAGE_BUCKET,
-//     projectId: process.env.FIREBASE_PROJECT_ID
-//   }
-
-//   if (!firebase.apps.length) {
-//     firebase.initializeApp(config)
-//   }
-// }
