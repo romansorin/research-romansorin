c6e98350b57fed314b0349414cffe3cec565892d
Roman Sorin <roman@romansorin.com>
2019-10-21 20:03:13 -0400

    Add button to test collection adds

--- src/pages/bibliography.js
+++ src/pages/bibliography.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import { database } from '../../firebase'
 
 const BibliographyPage = () => {
-  return <div>bib</div>
+  return (
+    <div>
+      <button
+        onClick={() => {
+          database
+            .collection('users')
+            .add({
+              first: 'Ada',
+              last: 'Lovelace',
+              born: 1815
+            })
+            .then(function (docRef) {
+              console.log('Document written with ID: ', docRef.id)
+            })
+            .catch(function (error) {
+              console.error('Error adding document: ', error)
+            })
+        }}
+      >
+        Add user
+      </button>
+    </div>
+  )
 }
 
 export default BibliographyPage
