b5dec6df9350caaf8fd5bbbb367304cc7ad84e30
Roman Sorin <roman@romansorin.com>
2019-10-21 20:54:23 -0400

    Comment config

--- firebase.js
+++ firebase.js
@@ -20,3 +20,21 @@ firebase.initializeApp(firebaseConfig)
 
 export const database = firebase.firestore()
 export const storage = firebase.storage()
+
+// Config from yawnch RFSSR
+// import firebase from 'firebase/app'
+// import 'firebase/auth'
+
+// export const initializeFirebase = () => {
+//   const config = {
+//     apiKey: process.env.FIREBASE_API_KEY,
+//     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//     databaseURL: process.env.FIREBASE_DATABASE_URL,
+//     // storageBucket: process.env.STORAGE_BUCKET,
+//     projectId: process.env.FIREBASE_PROJECT_ID
+//   }
+
+//   if (!firebase.apps.length) {
+//     firebase.initializeApp(config)
+//   }
+// }
