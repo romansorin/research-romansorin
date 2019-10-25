c161e88c9790f449a9b95e52ced21259a50440f8
Roman Sorin <roman@romansorin.com>
2019-10-24 23:47:24 -0400

    read example

--- src/pages/whitepaper.js
+++ src/pages/whitepaper.js
@@ -1,5 +1,17 @@
 import React from 'react'
 
+import { database } from '../../firebase'
+
+database
+  .collection('references')
+  .get()
+  .then(querySnapshot => {
+    querySnapshot.forEach(doc => {
+      console.log(`${doc.id} => ${doc.data()}`)
+      console.log(doc.data())
+    })
+  })
+
 const WhitepaperPage = () => {
   return <div />
 }
