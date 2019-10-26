c6c049fb183ac15f6096830061dceef641084975
Roman Sorin <988244@students.mentorschools.org>
2019-10-25 21:48:12 -0400

    Removed unused plugins and non-needed comments from gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,29 +28,15 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+        icon: 'src/images/gatsby-icon.png'
       }
     },
     'gatsby-plugin-postcss',
-    // {
-    //   resolve: 'gatsby-source-firebase',
-    //   options: {
-    //     // point to the firebase private key downloaded
-    //     credential: require('./firebase-key.json'),
-
-    //     // your firebase database root url
-    //     databaseURL: 'https://.../../..'
-    //   }
-    // },
     {
       resolve: 'gatsby-plugin-routes',
       options: {
-        // this is the path to your routes configuration file
         path: `${__dirname}/src/routes.js`
       }
     }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ]
 }
