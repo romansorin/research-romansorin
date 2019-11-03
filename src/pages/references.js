d5674dc1326ce574cf39bd228e4ed123fc322975
Roman Sorin <roman@romansorin.com>
2019-11-03 13:15:26 -0500

    Rename bib -> ref

---
+++ src/pages/references.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react'
+
+import { Reference } from '../models/Reference'
+
+const ReferencesPage = () => {
+  return <div />
+}
+
+export default ReferencesPage
--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,14 @@ const path = require('path')
 module.exports = [
   {
     path: '/citations',
-    component: path.resolve('src/pages/bibliography.js')
+    component: path.resolve('src/pages/references.js')
   },
   {
-    path: '/references',
-    component: path.resolve('src/pages/bibliography.js')
+    path: '/bibliography',
+    component: path.resolve('src/pages/references.js')
   },
   {
     path: '/sources',
-    component: path.resolve('src/pages/bibliography.js')
+    component: path.resolve('src/pages/references.js')
   }
 ]
