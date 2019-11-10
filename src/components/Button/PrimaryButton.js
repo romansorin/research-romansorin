df0954e4a5866e620b8d88dfe66488bc37184101
Roman Sorin <roman@romansorin.com>
2019-11-09 20:51:02 -0500

    fix

--- src/components/Button/PrimaryButton.js
+++ src/components/Button/PrimaryButton.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Button from './Button'
 
 const PrimaryButton = props => {
-  return <Button />
+  return <Button>{props.children}</Button>
 }
 
 export default PrimaryButton
--- src/components/Button/SecondaryButton.js
+++ src/components/Button/SecondaryButton.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Button from './Button'
 
 const SecondaryButton = props => {
-  return <Button />
+  return <Button>{props.children}</Button>
 }
 
 export default SecondaryButton
