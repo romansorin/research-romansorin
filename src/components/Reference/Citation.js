2b17d1a6717239411624b7bdb0292b6504b433e1
Roman Sorin <roman@romansorin.com>
2019-11-15 18:30:02 -0500

    more prelim styling.

--- src/components/Reference/Citation.js
+++ src/components/Reference/Citation.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const Citation = () => {
-  return <div />
+  return (
+    <div className='border-t border-white-2'>
+      <p className='py-2 font-light text-text-2'>
+        Citation of given article in IEEE format...
+      </p>
+    </div>
+  )
 }
 
 export default Citation
