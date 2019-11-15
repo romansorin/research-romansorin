1e652e1ca3fa7611611a63ceba7b06755e6838d8
Roman Sorin <988244@students.mentorschools.org>
2019-11-15 17:26:35 -0500

    Empty components

---
+++ src/components/Reference/Heading.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Heading = () => {
+  return <div />
+}
+
+export default Heading
---
+++ src/components/Reference/Main.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Main = () => {
+  return <main />
+}
+
+export default Main
