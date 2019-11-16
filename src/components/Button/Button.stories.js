d0e963049af0a8231b32a9cc8f5e4edcb15b269f
Roman Sorin <roman@romansorin.com>
2019-11-15 21:13:00 -0500

    Button styling

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,39 +1,62 @@
 import React from 'react'
-import { action } from '@storybook/addon-actions'
 
-// import { Button, PrimaryButton, SecondaryButton } from './index'
-import Button from './Button'
+import { Button, PrimaryButton, SecondaryButton } from '.'
 
 export default {
   title: 'Button'
 }
 
-export const base = () => (
-  <div className='mx-5 my-5'>
-    <Button className='bg-black text-white-0' onClick={action('clicked')}>
-      Button
-    </Button>
+const commonStyles = 'mr-8'
+const hoverShadowStyle = 'shadow-1'
+
+const Row = props => (
+  <div>
+    <h1 className='text-xl font-semibold mb-4'>{props.info}</h1>
+    <div className='flex flex-row justify-start mb-8'>{props.children}</div>
   </div>
 )
 
-// export const primary = () => (
-//   <div className='mx-5 my-5'>
-//     <PrimaryButton
-//       className='bg-black text-white-0'
-//       onClick={action('clicked')}
-//     >
-//       Button
-//     </PrimaryButton>
-//   </div>
-// )
+export const base = () => (
+  <div className='mx-5 my-5'>
+    <Button>Button</Button>
+  </div>
+)
 
-// export const secondary = () => (
-//   <div className='mx-5 my-5'>
-//     <SecondaryButton
-//       className='bg-black text-white-0'
-//       onClick={action('clicked')}
-//     >
-//       Button
-//     </SecondaryButton>
-//   </div>
-// )
+export const primary = () => (
+  <div className='mx-5 my-5'>
+    <Row info='Variant 0'>
+      <PrimaryButton className={`${commonStyles}`} variant={0}>
+        Button
+      </PrimaryButton>
+    </Row>
+    <Row info='Variant 1'>
+      <PrimaryButton className={`${commonStyles}`} variant={1}>
+        Button
+      </PrimaryButton>
+    </Row>
+    <Row info='Variant 2'>
+      <PrimaryButton className={`${commonStyles}`} variant={2}>
+        Button
+      </PrimaryButton>
+    </Row>
+    <Row info='Variant 3'>
+      <PrimaryButton className={`${commonStyles}`} variant={3}>
+        Button
+      </PrimaryButton>
+    </Row>
+  </div>
+)
+
+export const secondary = () => (
+  <div className='mx-5 my-5'>
+    <Row info='Variant 0'>
+      <SecondaryButton variant={0}>Button</SecondaryButton>
+    </Row>
+    <Row info='Variant 1'>
+      <SecondaryButton variant={1}>Button</SecondaryButton>
+    </Row>
+    <Row info='Variant 2'>
+      <SecondaryButton variant={2}>Button</SecondaryButton>
+    </Row>
+  </div>
+)
--- src/components/Button/PrimaryButton.js
+++ src/components/Button/PrimaryButton.js
@@ -2,8 +2,53 @@ import React from 'react'
 
 import Button from './Button'
 
+const commonStyles = 'text-white-0'
+
 const PrimaryButton = props => {
-  return <Button>{props.children}</Button>
+  switch (props.variant) {
+    case 0:
+      return (
+        <Button
+          className={`${
+            props.className
+          } ${commonStyles} bg-theme-2 hover:bg-theme-5 focus:bg-theme-5`}
+        >
+          {props.children}
+        </Button>
+      )
+    case 1:
+      return (
+        <Button
+          className={`${
+            props.className
+          } ${commonStyles} bg-theme-5 hover:bg-theme-4 focus:bg-theme-4`}
+        >
+          {props.children}
+        </Button>
+      )
+    case 2:
+      return (
+        <Button
+          className={`${
+            props.className
+          } ${commonStyles} bg-theme-0 hover:bg-theme-1 focus:bg-theme-1`}
+        >
+          {props.children}
+        </Button>
+      )
+    case 3:
+      return (
+        <Button
+          className={`${
+            props.className
+          } ${commonStyles} bg-text-2 hover:bg-text-1 focus:bg-text-1`}
+        >
+          {props.children}
+        </Button>
+      )
+    default:
+      return <Button>{props.children}</Button>
+  }
 }
 
 export default PrimaryButton
