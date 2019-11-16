9ea0ca5053b66a611fc2f98124580b305589c59b
Roman Sorin <roman@romansorin.com>
2019-11-15 22:22:18 -0500

    Btn

--- src/components/Reference/Aside.js
+++ src/components/Reference/Aside.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
+import PrimaryButton from '../Button/PrimaryButton'
+
 const Aside = () => {
   return (
     <aside>
       <p className='text-text-2 font-bold'>NOV 1, 2019</p>
-      <button>DOWNLOAD</button>
+      <a href='/'>
+        <PrimaryButton variant={0}>DOWNLOAD</PrimaryButton>
+      </a>
     </aside>
   )
 }
