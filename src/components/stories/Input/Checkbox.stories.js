04f7bd85f049fda3a8c0efff3c414db218182d8b
Roman Sorin <roman@romansorin.com>
2019-11-17 09:27:49 -0500

    nonworking checkbox

--- src/components/stories/Input/Checkbox.stories.js
+++ src/components/stories/Input/Checkbox.stories.js
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { Wrapper, Row } from '../layout'
+import Checkbox from '../../Checkbox/Checkbox'
 
 export default { title: 'Input/Checkbox' }
 
-export const unchecked = () => <div />
-export const checked = () => <div />
+export const base = () => {
+  const [checked, setChecked] = useState(false)
+  return (
+    <Wrapper>
+      <Row info='Unchecked'>
+        <Checkbox
+          name='checkbox-1'
+          id='checkbox-1'
+          checked={checked}
+          onChange={() => setChecked(!checked)}
+          height={24}
+          width={24}
+          label='Checkbox'
+        />
+      </Row>
+      <Row info='Checked'>
+        <Checkbox height={24} width={24} label='Checkbox' />
+      </Row>
+    </Wrapper>
+  )
+}
---
+++ src/icons/index.js
@@ -0,0 +1,6 @@
+export { default as CheckboxChecked } from './Checkbox/CheckboxChecked'
+export { default as CheckboxUnchecked } from './Checkbox/CheckboxUnchecked'
+
+export { default as RadioFilled } from './Radio/RadioFilled'
+export { default as RadioHover } from './Radio/RadioHover'
+export { default as RadioUnfilled } from './Radio/RadioUnfilled'
