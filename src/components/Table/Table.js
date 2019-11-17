8ddec3b670977a5db8bf9be97e76b94e00607f15
Roman Sorin <roman@romansorin.com>
2019-11-17 10:21:51 -0500

    Table

---
+++ src/components/Table/Table.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Table = () => {
+  return (
+    <table>
+      <tr>
+        <td>Paper</td>
+        <td>Paper</td>
+        <td>Paper</td>
+      </tr>
+    </table>
+  )
+}
+
+export default Table
---
+++ src/components/stories/Table.stories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Wrapper, Row } from './layout'
+
+export default {
+  title: 'Table'
+}
+
+export const minimal = () => (
+  <Wrapper>
+    <Row info='Minimal'>
+      <div />
+    </Row>
+  </Wrapper>
+)
