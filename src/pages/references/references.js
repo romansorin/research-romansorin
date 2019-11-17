30fb2d21c90159d057f678a0eb151c1d79d9b9e1
Roman Sorin <roman@romansorin.com>
2019-11-17 11:38:53 -0500

    ref?

--- src/pages/references/references.js
+++ src/pages/references/references.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { getAllReferences } from '../../models/Reference'
 import Layout from '../../components/layout'
 import SEO from '../../components/seo'
