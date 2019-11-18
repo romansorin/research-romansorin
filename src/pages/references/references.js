506f396a29397b87370dd6ef2265a4171ea875ce
Roman Sorin <988244@students.mentorschools.org>
2019-11-18 14:56:24 -0500

    references table

--- src/pages/references/references.js
+++ src/pages/references/references.js
@@ -1,89 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Layout, SEO } from 'Components'
 import { LiaInput } from 'Components/Input'
 import { Table } from 'Components/Table'
 import { PrimaryButton } from 'Components/Button'
 
-const tableData = {
-  rows: [
-    {
-      className: 'w-full flex',
-      columns: [
-        { renderAs: 'text', className: 'pl-1 mr-auto', text: 'Paper' },
-        {
-          className: 'ml-4 md:ml-0 mr-auto',
-          renderAs: 'text',
-          text: 'Author'
-        },
-        {
-          className: 'ml-auto mr-3 uppercase font-bold text-theme-2',
-          renderAs: 'Link',
-          to: 'https://romansorin.com',
-          text: 'View'
-        }
-      ]
-    },
-    {
-      className: 'w-full flex',
-      columns: [
-        { renderAs: 'text', className: 'pl-1 mr-auto', text: 'Paper' },
-        {
-          className: 'ml-4 md:ml-0 mr-auto',
-          renderAs: 'text',
-          text: 'Author'
-        },
-        {
-          className: 'ml-auto mr-3 uppercase font-bold text-theme-2',
-          renderAs: 'Link',
-          to: 'https://romansorin.com',
-          text: 'View'
-        }
-      ]
-    },
-    {
-      className: 'w-full flex',
-      columns: [
-        { renderAs: 'text', className: 'pl-1 mr-auto', text: 'Paper' },
-        {
-          className: 'ml-4 md:ml-0 mr-auto',
-          renderAs: 'text',
-          text: 'Author'
-        },
-        {
-          className: 'ml-auto mr-3 uppercase font-bold text-theme-2',
-          renderAs: 'Link',
-          to: 'https://romansorin.com',
-          text: 'View'
-        }
-      ]
-    },
-    {
-      className: 'w-full flex',
-      columns: [
-        { renderAs: 'text', className: 'pl-1 mr-auto', text: 'Paper' },
-        {
-          className: 'ml-4 md:ml-0 mr-auto',
-          renderAs: 'text',
-          text: 'Author'
-        },
-        {
-          className: 'ml-auto mr-3 uppercase font-bold text-theme-2',
-          renderAs: 'Link',
-          to: 'https://romansorin.com',
-          text: 'View'
-        }
-      ]
-    }
-  ]
+import { Database } from 'Firebase'
+
+const PATH = 'references'
+const DB = Database.collection(PATH)
+
+const columnStyles = {
+  left: 'pl-1 mr-auto',
+  middle: 'ml-4 md:ml-0 mr-auto',
+  right: 'ml-auto mr-3 uppercase font-bold text-theme-2'
+}
+const renderAs = {
+  TEXT: 'text',
+  LINK: 'Link'
 }
 
 const ReferencesPage = () => {
-  // useEffect(() => {
-  //   const refs = getAllReferences().then(res =>
-  //     res.forEach(doc => console.log(doc.data()))
-  //   )
-  // }, [])
+  const [references, setReferences] = useState()
+
+  const setRows = data => {
+    const rows = []
+    data.map(doc => {
+      rows.push({
+        columns: [
+          {
+            renderAs: renderAs.TEXT,
+            className: columnStyles.left,
+            text: doc.title
+          },
+          {
+            renderAs: renderAs.TEXT,
+            className: columnStyles.middle,
+            text: doc.authors.map((author, i) => {
+              if (i < doc.authors.length - 1) return `${author}, `
+              else return author
+            })
+          },
+          {
+            renderAs: renderAs.LINK,
+            to: doc.slug,
+            className: columnStyles.right,
+            text: 'View'
+          }
+        ]
+      })
+    })
+    return setReferences({ rows: rows })
+  }
+  useEffect(
+    () => {
+      const tempData = []
+      DB.limit(5)
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => tempData.push(doc.data()))
+          setRows(tempData)
+        })
+    },
+    [renderAs, columnStyles]
+  )
+
   return (
     <Layout>
       <SEO title='All references' />
@@ -91,7 +72,16 @@ const ReferencesPage = () => {
         References
       </h1>
       <LiaInput placeholder='Paper title or author' />
-      <Table className='mx-auto w-11/12 md:w-full mt-10 mb-20' {...tableData} />
+
+      {references && references.rows.length > 0 ? (
+        <Table
+          className='mx-auto w-11/12 md:w-full mt-10 mb-20'
+          {...references}
+        />
+      ) : (
+        ''
+      )}
+
       <PrimaryButton className='px-10 mx-auto flex' variant={0}>
         Load more
       </PrimaryButton>
