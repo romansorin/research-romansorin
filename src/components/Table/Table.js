8cbcad810d13a43cc1ef9a00ed33acad7a06097b
Roman Sorin <988244@students.mentorschools.org>
2019-11-18 08:48:46 -0500

    Added <tbody> to table

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -4,40 +4,42 @@ import { Link } from 'gatsby'
 const Table = props => {
   return (
     <table className={`${props.className ? props.className + ' ' : ''}`}>
-      {props.rows.map((row, i) => (
-        <tr
-          className={`${
-            row.className ? row.className + ' ' : ''
-          }odd:bg-white-1 even:bg-white-0 py-4`}
-          key={i}
-        >
-          {row.columns.map((column, j) => (
-            <td
-              className={`${column.className ? column.className + ' ' : ''}${
-                column.renderAs === 'text' ? 'text-text-2' : ''
-              }`}
-              key={j}
-            >
-              {column.renderAs === 'a' ? (
-                <a href={column.href}>{column.text}</a>
-              ) : (
-                ''
-              )}
-              {column.renderAs === 'Link' ? (
-                <Link to={column.to}>{column.text}</Link>
-              ) : (
-                ''
-              )}
-              {column.renderAs === 'button' ? (
-                <button type={column.buttonType}>{column.text}</button>
-              ) : (
-                ''
-              )}
-              {column.renderAs === 'text' ? column.text : ''}
-            </td>
-          ))}
-        </tr>
-      ))}
+      <tbody>
+        {props.rows.map((row, i) => (
+          <tr
+            className={`${
+              row.className ? row.className + ' ' : ''
+            }odd:bg-white-1 even:bg-white-0 py-4 w-full flex`}
+            key={i}
+          >
+            {row.columns.map((column, j) => (
+              <td
+                className={`${column.className ? column.className + ' ' : ''}${
+                  column.renderAs === 'text' ? 'text-text-2' : ''
+                }`}
+                key={j}
+              >
+                {column.renderAs === 'a' ? (
+                  <a href={column.href}>{column.text}</a>
+                ) : (
+                  ''
+                )}
+                {column.renderAs === 'Link' ? (
+                  <Link to={column.to}>{column.text}</Link>
+                ) : (
+                  ''
+                )}
+                {column.renderAs === 'button' ? (
+                  <button type={column.buttonType}>{column.text}</button>
+                ) : (
+                  ''
+                )}
+                {column.renderAs === 'text' ? column.text : ''}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
     </table>
   )
 }
