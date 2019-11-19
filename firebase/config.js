75871726a5de91fb6a329160eba268af5a14f00f
Roman Sorin <roman@romansorin.com>
2019-11-18 23:16:06 -0500

    algolia init

--- firebase/config.js
+++ firebase/config.js
@@ -5,6 +5,15 @@ import 'firebase/firestore'
 import 'firebase/analytics'
 import 'firebase/storage'
 
+const algoliasearch = require('algoliasearch')
+
+const algolia = algoliasearch(
+  process.env.ALGOLIA_APP_ID,
+  process.env.ALGOLIA_API_KEY
+)
+
+const index = algolia.initIndex(process.env.ALGOLIA_INDEX_NAME)
+
 const config = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
