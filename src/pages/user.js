8c3c5ae4f8fa208ccc25063f211655c5cbb3d4dc
Roman Sorin <roman@romansorin.com>
2019-12-29 04:53:29 -0500

    wip

--- src/pages/user.js
+++ src/pages/user.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { Auth } from 'Firebase/'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 
 const email = 'roman@romansorin.com'
 const pass = 'password'
@@ -7,12 +9,17 @@ const pass = 'password'
 const User = () => {
   const [loggedIn, setLoggedIn] = useState('false')
 
+  const res = Auth.currentUser
+
   const createUser = async () => {
     const response = await Auth.signInWithEmailAndPassword(email, pass).catch(
       error => console.log(error)
     )
     // const response = await Auth.signOut()
     if (response.user) setLoggedIn('true')
+    Auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL).then(() =>
+      console.log('set persistence to session')
+    )
   }
   return (
     <div>
