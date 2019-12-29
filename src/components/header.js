55c04cd41eb5f7ab9fd44776507b32bcaa5f8019
Roman Sorin <roman@romansorin.com>
2019-12-29 00:44:45 -0500

    wip

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,44 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import PropTypes from 'prop-types'
+import React from "react"
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import { SecondaryButton } from "Components/Button"
 
 const Header = ({ siteTitle }) => (
-  <header className='py-8 px-8 tracking-tight text-xl'>
+  <header className='py-5 px-8 text-lg'>
     <div className='container mx-auto flex flex-row'>
-      <Link className='ml-auto hover:text-theme-2 text-text-3' to='/references'>
+      <Link className='hover:text-theme-2 my-auto mr-auto text-text-3' to='/'>
+        Research
+      </Link>
+      <Link
+        className='hover:text-theme-2 my-auto mx-5 text-text-3'
+        to='/whitepaper'
+      >
+        Whitepaper
+      </Link>
+      <Link
+        className='hover:text-theme-2 my-auto mx-5 text-text-3'
+        to='/references'
+      >
         References
+      </Link>
+      <Link className='hover:text-theme-2 my-auto mx-5 text-text-3' to='/blog'>
+        Blog
+      </Link>
+      <Link to='/paper'>
+        <SecondaryButton className='px-6 ml-4' variant={0}>
+          Paper
+        </SecondaryButton>
       </Link>
     </div>
   </header>
 )
 
 Header.propTypes = {
-  siteTitle: PropTypes.string
+  siteTitle: PropTypes.string,
 }
 
 Header.defaultProps = {
-  siteTitle: ''
+  siteTitle: "",
 }
 
 export default Header
--- src/pages/whitepaper.js
+++ src/pages/whitepaper.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react"
+import { Layout, SEO } from "Components/"
 
 const WhitepaperPage = () => {
-  return <div />
+  return (
+    <Layout>
+      <SEO title='Home' />
+      <div className='flex px-3 my-64'>
+        <h1 className='text-center mx-auto md:text-3xl text-2xl'>
+          In progress. For inquiries, email{" "}
+          <a
+            className='text-indigo-500 hover:text-gray-900'
+            href='mailto:roman@romansorin.com'
+          >
+            roman@romansorin.com
+          </a>
+        </h1>
+      </div>
+    </Layout>
+  )
 }
 
 export default WhitepaperPage
