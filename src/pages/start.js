9de7c9ba346a3429de41f046a84b1af9e1d6675f
Roman Sorin <roman@romansorin.com>
2020-01-22 15:37:28 -0500

    added a start page placeholder for the app

---
+++ src/pages/start.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Layout } from 'Components'
+
+const StartPage = () => {
+  return <Layout>Start/consent page for application & experiment.</Layout>
+}
+
+export default StartPage
