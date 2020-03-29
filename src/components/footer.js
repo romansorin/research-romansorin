229d1452e7cfd7cdaea203b81533b14a076ebaf0
Roman Sorin <rmaximsorin@gmail.com>
2020-03-29 17:26:23 -0400

    tmpl footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
-const Footer = () => <footer className='py-5 px-8 text-lg'>d</footer>
+const Footer = () => (
+  <footer className='py-5 px-8 text-lg'>
+    <div>Logo Whitepaper References Blog ICONICONICONICON</div>
+    <div>
+      © 2020 Sorin Studio. All rights reserved. Cookies Privacy Policy Terms of
+      Service
+    </div>
+  </footer>
+)
 
 export default Footer
