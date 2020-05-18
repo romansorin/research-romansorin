e96fb655987d645124a6f93e4802b1e87756ec47
Roman Sorin <rmaximsorin@gmail.com>
2020-05-18 19:29:48 -0400

    add in hotjar script

--- src/components/seo.js
+++ src/components/seo.js
@@ -61,25 +61,7 @@ function SEO ({ description, lang, meta, title }) {
           content: metaDescription
         }
       ].concat(meta)}
-    >
-      {/* <script>
-        `
-        {(function (h, o, t, j, a, r) {
-          h.hj =
-            h.hj ||
-            function () {
-              ;(h.hj.q = h.hj.q || []).push(arguments)
-            }
-          h._hjSettings = { hjid: 1327317, hjsv: 6 }
-          a = o.getElementsByTagName('head')[0]
-          r = o.createElement('script')
-          r.async = 1
-          r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
-          a.appendChild(r)
-        })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')}
-        `
-      </script> */}
-    </Helmet>
+    ></Helmet>
   )
 }
 
