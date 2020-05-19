be107ab99e6b8cb00f2aed0e0158b5ecdd492243
Roman Sorin <roman@romansorin.com>
2020-05-18 21:10:06 -0400

    fixes

--- src/pages/finished.js
+++ src/pages/finished.js
@@ -1,15 +1,35 @@
 import React, { useEffect } from 'react'
 
 import { Database } from 'Firebase'
+import { Layout, SEO } from 'Components/index'
 
 const FinishedPage = props => {
+  if (!props.location.search.substring(5).length > 0)
+    return <div>Missing document ref.</div>
   useEffect(() => {
-    Database.collection('users')
-      .doc(props.location.search.substring(5))
-      .update({ session_end: new Date() })
-  })
+    if (props.location.search.substring(5).length > 0) {
+      Database.collection('users')
+        .doc(props.location.search.substring(5))
+        .update({ session_end: new Date() })
+    }
+  }, [])
 
-  return <div>Finished</div>
+  return (
+    <Layout>
+      <SEO title='Finished' />
+      <div className='flex px-3 my-64'>
+        <h1 className='mx-auto text-2xl text-center md:text-3xl'>
+          Thank you for participating. For inquiries, email{' '}
+          <a
+            className='text-indigo-500 hover:text-gray-900'
+            href='mailto:roman@romansorin.com'
+          >
+            roman@romansorin.com
+          </a>
+        </h1>
+      </div>
+    </Layout>
+  )
 }
 
 export default FinishedPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,14 @@ const pages = ['8nqXhdl3JD8u', 'hwVB0eKUehxy', 'vtc5qYP2r8Ut']
 
 const IndexPage = () => {
   const [email, setEmail] = useState(null)
+  const [emailError, setEmailError] = useState(false)
 
   const beginExperiment = () => {
+    if (!email) {
+      setEmailError(true)
+      return
+    } else setEmailError(false)
+
     const randomVariant = pages[Math.floor(Math.random() * 3)]
     Database.collection('users')
       .add({
@@ -28,18 +34,41 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title='Start' />
-      <section className='lg:block hidden'>
+      <section className='hidden lg:block'>
         <h1 className='mb-3 text-3xl font-medium leading-snug text-text-2 md:text-4xl'>
           Welcome to my research study.
         </h1>
         <p>
-          A little bit of info on what this is about, how long it will take,
-          etc. Directions: Goal: Please reach registration of service presented.
+          This study is centered around identifying how different common or
+          popular layouts may potentially yield more effective retention or
+          customer acquisition, and detail other associated behaviors. In
+          summary, three common layouts were identified and created from six
+          clusters of almost 1000 sites, identified by a K-Means clustering
+          algorithm.
+          <br />
+          <br />
+          This experiment should take approximately three minutes or less.
+          Please navigate within the layout and attempt to identify how to begin
+          using or signing up for the presented product. None of the copy or
+          images represent any real companies or products in any aspect.
+          <br />
+          <br />
+          Emails are collected strictly for unique identification and filtering
+          responses. Courtesy of{' '}
+          <a
+            className='font-semibold text-theme-1'
+            href='https://tailwindui.com'
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            TailwindUI
+          </a>{' '}
+          for providing the components to build out sample layouts. You must use
+          a desktop device to participate.
         </p>
         <Row className='flex-wrap my-6'>
           <Column className='w-full my-6 md:my-0 md:w-1/2 md:pr-12'>
-            <Label htmlFor='title'>Title</Label>
-
+            <Label htmlFor='email'>Email address</Label>
             <Input
               onChange={event => setEmail(event.target.value)}
               value={email}
@@ -49,13 +78,13 @@ const IndexPage = () => {
               name='email'
               className='mt-3'
             />
+            {emailError && (
+              <small className='mt-1 text-invalid'>
+                You must provide an email.
+              </small>
+            )}
           </Column>
         </Row>
-        <input type='checkbox' name='agree' />
-        <label for='agree'>
-          By checking this box, you acknowledge that you have read the data
-          usage and privacy policy clauses, and agree to the terms of service.{' '}
-        </label>
         <PrimaryButton
           variant={0}
           onClick={() => beginExperiment()}
@@ -65,7 +94,7 @@ const IndexPage = () => {
         </PrimaryButton>
       </section>
 
-      <div className='lg:hidden block container mx-auto'>
+      <div className='container block mx-auto lg:hidden'>
         A desktop device is required to participate.
       </div>
     </Layout>
